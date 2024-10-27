2db8aa3d93c7a4bb0a2126725656633d8910e787
hugo <devhugok@gmail.com>
2024-10-27 21:51:24 +0100

    add fixture to create fake property data

--- src/DataFixtures/PropertyFixture.php
+++ src/DataFixtures/PropertyFixture.php
@@ -12,23 +12,26 @@ class PropertyFixture extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr_FR');
-    for($i =0; $i < 100; $i++){
-        $property = new Property();
-        $property
-            ->setTitle($faker->words(3,true))
-            ->setDescription($faker->words(3,true))
-            ->setSurface($faker->numberBetween(20,350))
-            ->setRooms($faker->numberBetween(2,10))
-            ->setBedrooms($faker->numberBetween(1,9))
-            ->setFloor($faker->numberBetween(0,15))
-            ->setPrice($faker->numberBetween(100000,1000000))
-            ->setHeat($faker->numberBetween(0,count(Property::HEAT) - 1))
-            ->setCity($faker->city)
-            ->setAddress($faker->address)
-            ->setPostalCode($faker->postcode)
-            ->setSold(false);
-        $manager->persist($property);
-    }
+        for($i =0; $i < 100; $i++){
+            $property = new Property();
+            $property
+                ->setTitle($faker->words(3,true))
+                ->setDescription($faker->words(3,true))
+                ->setSurface($faker->numberBetween(20,350))
+                ->setRooms($faker->numberBetween(2,10))
+                ->setBedrooms($faker->numberBetween(1,9))
+                ->setFloor($faker->numberBetween(0,15))
+                ->setPrice($faker->numberBetween(100000,1000000))
+                ->setHeat($faker->numberBetween(0,count(Property::HEAT) - 1))
+                ->setCity($faker->city)
+                ->setAddress($faker->address)
+                ->setPostalCode($faker->postcode)
+                ->setSold(false)
+                ->setCreatedAt($faker->dateTime)
+                ->setFilename("default.png")
+                ->setUpdatedAt($faker->dateTime);
+            $manager->persist($property);
+        }
     $manager->flush();
     }
 }
