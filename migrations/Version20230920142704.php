1f610fc94294c593641b5bc00aca63d12e8c4d72
hugo <devhugok@gmail.com>
2026-07-05 21:40:33 +0200

    update to symfony 5.4

--- migrations/Version20230920142704.php
+++ migrations/Version20230920142704.php
@@ -12,20 +12,20 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20230920142704 extends AbstractMigration
 {
-    public function getDescription(): string
-    {
-        return '';
-    }
+  public function getDescription(): string
+  {
+    return '';
+  }
 
-    public function up(Schema $schema): void
-    {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE property ADD file_name VARCHAR(255) NOT NULL');
-    }
+  public function up(Schema $schema): void
+  {
+    // this up() migration is auto-generated, please modify it to your needs
+    $this->addSql('ALTER TABLE property ADD file_name VARCHAR(255) DEFAULT NULL');
+  }
 
-    public function down(Schema $schema): void
-    {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE property DROP file_name');
-    }
+  public function down(Schema $schema): void
+  {
+    // this down() migration is auto-generated, please modify it to your needs
+    $this->addSql('ALTER TABLE property DROP file_name');
+  }
 }
